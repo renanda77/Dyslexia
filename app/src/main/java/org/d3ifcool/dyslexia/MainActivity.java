851584f974765c20c08769bff5175e88df423846
Renanda Tribowo <renanda.tribowo7@gmail.com>
2019-02-20 23:26:56 +0700

    Update MainActivity.java

--- app/src/main/java/org/d3ifcool/dyslexia/MainActivity.java
+++ app/src/main/java/org/d3ifcool/dyslexia/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // check
 
     mBaca = findViewById(R.id.imgMembaca);
     mBaca.setOnClickListener(new View.OnClickListener() {
