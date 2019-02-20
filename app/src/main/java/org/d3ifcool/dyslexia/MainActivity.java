95d99914151bc2f9914d0cfeb05bbecc7e937e9c
aldiniskandar <nabilap8@gmail.com>
2019-02-20 15:11:16 +0700

    Add Quiz

--- app/src/main/java/org/d3ifcool/dyslexia/MainActivity.java
+++ app/src/main/java/org/d3ifcool/dyslexia/MainActivity.java
@@ -1,13 +1,32 @@
 package org.d3ifcool.dyslexia;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mBaca;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    mBaca = findViewById(R.id.imgMembaca);
+    mBaca.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent iMembaca = new Intent(getApplicationContext(),MembacaActivity.class);
+            startActivity(iMembaca);
+        }
+    });
+
     }
 }
